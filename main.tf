6b5dc8ea0f5c0197e0d6af6cc485b899efa2cafa
Bhuwan Negi <Bhuwansingh.Negi1@outlook.com>
2022-01-15 10:54:44 +0530

    Committed again

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  required_providers {
+    azurerm = "<=2.80.0"
+  }
+}
+
+resource "azurerm_resource_group" "Example1"{
+	name = "Bhuwan-rg"
+	location = "West Europe"
+}
