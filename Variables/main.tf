1954f7d0f2b0c78d32a116cacf4557f3943106a4
Bhuwan Negi <Bhuwansingh.Negi1@outlook.com>
2022-01-22 12:02:12 +0530

    new committ

---
+++ Variables/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = "<=2.91.0"
+  }
+}
+
+resource "azurerm_resource_group" "Example1"{
+	name = "Bhuwan-rg"
+	location = "West Europe"
+}
+
+resource "azurerm_resource_group" "Example2"{
+	name = "Beautiful-rg"
+	location = "West Europe"
+}
